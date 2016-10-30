124306636681c9f9834c94dc63d9a6ee4fd1eea0
AnilDeshpande <anildesh82@gmail.com>
2016-10-30 09:50:31 +0530

    with button added to have more controlled load of data and also added usage of Loader
    A button has been added to the UI and also the Loader has been used to
    load the contacts more efficiently in the activity.

--- app/src/main/java/codetutor/youtube/edu/contentproviderdemo/MainActivity.java
+++ app/src/main/java/codetutor/youtube/edu/contentproviderdemo/MainActivity.java
@@ -1,19 +1,25 @@
 package codetutor.youtube.edu.contentproviderdemo;
 
+
+import android.app.LoaderManager;
 import android.content.ContentResolver;
 import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener{
 
     private static final String TAG="ContentProviderDemo";
 
     private TextView textViewQueryResult;
+    private Button buttonLoadData;
 
     private String [] mColumnProjection=new String[]{
             ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
@@ -33,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
         textViewQueryResult=(TextView)findViewById(R.id.textViewQueryResult);
 
-        ContentResolver contentResolver=getContentResolver();
+        buttonLoadData=(Button)findViewById(R.id.buttonLoadData);
+        buttonLoadData.setOnClickListener(this);
+
+        /*ContentResolver contentResolver=getContentResolver();
         Cursor cursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
                 mColumnProjection,
                 null,
@@ -48,6 +57,44 @@ public class MainActivity extends AppCompatActivity {
             textViewQueryResult.setText(stringBuilderQueryResult.toString());
         }else{
             textViewQueryResult.setText("No Contacts in device");
+        }*/
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        if(id==1){
+            return  new CursorLoader(MainActivity.this,ContactsContract.Contacts.CONTENT_URI,
+                    mColumnProjection,null,null,null);
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        if(cursor!=null && cursor.getCount()>0){
+            StringBuilder stringBuilderQueryResult=new StringBuilder("");
+            while (cursor.moveToNext()){
+                stringBuilderQueryResult.append(cursor.getString(0)+" , "+cursor.getString(1)+" , "+cursor.getString(2)+"\n");
+            }
+            textViewQueryResult.setText(stringBuilderQueryResult.toString());
+        }else{
+            textViewQueryResult.setText("No Contacts in device");
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonLoadData: getLoaderManager().initLoader(1,null,this);
+                break;
+            default:break;
         }
     }
 }
