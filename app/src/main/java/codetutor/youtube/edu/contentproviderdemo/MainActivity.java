f33ae414dff07c6a2e65685005ae78f1c5bec057
AnilDeshpande <anildesh82@gmail.com>
2016-11-14 06:53:23 +0530

    This is a new branch with all remaining features

--- app/src/main/java/codetutor/youtube/edu/contentproviderdemo/MainActivity.java
+++ app/src/main/java/codetutor/youtube/edu/contentproviderdemo/MainActivity.java
@@ -3,12 +3,18 @@ package codetutor.youtube.edu.contentproviderdemo;
 
 import android.Manifest;
 import android.app.LoaderManager;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
+import android.content.OperationApplicationException;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.RemoteException;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -17,26 +23,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 import android.support.design.widget.Snackbar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "ContentProviderDemo";
 
-    private int MY_PERMISSIONS_REQUEST_READ_CONTACTS=20;
+    //private int MY_PERMISSIONS_REQUEST_READ_CONTACTS=20;
 
     private boolean firstTimeLoaded=false;
 
     private TextView textViewQueryResult;
-    private Button buttonLoadData;
+    private Button buttonLoadData, buttonAddContact,buttonRemoveContact,buttonUpdateContact;
+
+    private ContentResolver contentResolver;
+
+    private EditText editTextContactName;
+    private CursorLoader mContactsLoader;
 
     private String[] mColumnProjection = new String[]{
+            ContactsContract.Contacts._ID,
             ContactsContract.Contacts.DISPLAY_NAME_PRIMARY,
             ContactsContract.Contacts.CONTACT_STATUS,
             ContactsContract.Contacts.HAS_PHONE_NUMBER};
@@ -59,8 +75,24 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         textViewQueryResult = (TextView) findViewById(R.id.textViewQueryResult);
 
+        editTextContactName=(EditText)findViewById(R.id.editTextContactName);
+
         buttonLoadData = (Button) findViewById(R.id.buttonLoadData);
+
+        buttonAddContact=(Button)findViewById(R.id.buttonAddContact);
+        buttonRemoveContact=(Button)findViewById(R.id.buttonRemoveContact);
+        buttonUpdateContact=(Button)findViewById(R.id.buttonUpdateContact);
+
+
+
         buttonLoadData.setOnClickListener(this);
+
+        buttonAddContact.setOnClickListener(this);
+        buttonRemoveContact.setOnClickListener(this);
+        buttonUpdateContact.setOnClickListener(this);
+
+
+        contentResolver=getContentResolver();
 
         /*ContentResolver contentResolver=getContentResolver();
         Cursor cursor=contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
@@ -82,27 +114,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     }
 
-
     @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-
-        if (id == 1) {
-            return new CursorLoader(MainActivity.this, ContactsContract.Contacts.CONTENT_URI,
-                    mColumnProjection, null, null, null);
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        if(i==1){
+            return  new CursorLoader(this,ContactsContract.Contacts.CONTENT_URI,mColumnProjection, null,null,null);
         }
         return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        if (cursor != null && cursor.getCount() > 0) {
-            StringBuilder stringBuilderQueryResult = new StringBuilder("");
-            //cursor.moveToFirst();
-            while (cursor.moveToNext()) {
-                stringBuilderQueryResult.append(cursor.getString(0) + " , " + cursor.getString(1) + " , " + cursor.getString(2) + "\n");
+        if(cursor!=null && cursor.getCount()>0){
+            StringBuilder stringBuilderQueryResult=new StringBuilder("");
+            while (cursor.moveToNext()){
+                stringBuilderQueryResult.append(cursor.getString(0)+" , "+cursor.getString(1)+" , "+cursor.getString(2)+" , "+cursor.getString(3)+"\n");
             }
             textViewQueryResult.setText(stringBuilderQueryResult.toString());
-        } else {
+        }else{
             textViewQueryResult.setText("No Contacts in device");
         }
     }
@@ -115,17 +143,107 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.buttonLoadData: if(firstTimeLoaded==false){
-                getLoaderManager().initLoader(1, null, this);
-                firstTimeLoaded=true;
-            }else{
-                getLoaderManager().restartLoader(1,null,this);
-            }
+            case R.id.buttonLoadData:
+                if(firstTimeLoaded==false){
+                    getLoaderManager().initLoader(1, null,this);
+                    firstTimeLoaded=true;
+                }else{
+                    getLoaderManager().restartLoader(1,null,this);
+                }
 
+                break;
+            case R.id.buttonAddContact: addContact();
+                break;
+            case R.id.buttonRemoveContact:removeContacts();
+                break;
+            case R.id.buttonUpdateContact: updateContact();
                 break;
             default:
                 break;
         }
+   }
+    private void addContact() {
+        ArrayList<ContentProviderOperation> cops=new ArrayList<ContentProviderOperation>();
+
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE,"accountname@gmail.com")
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, "com.google")
+                .build());
+        cops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, editTextContactName.getText().toString())
+                .build());
+
+        try{
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY,cops);
+        }catch (Exception exception){
+            Log.i(TAG,exception.getMessage());
+        }
     }
+
+
+    private void updateContact(){
+
+        String [] updateValue=editTextContactName.getText().toString().split(" ");
+        ContentProviderResult[] result=null;
+
+        String targetString=null;
+        String newString=null;
+        if(updateValue.length==2){
+
+            targetString=updateValue[0];
+            newString=updateValue[1];
+
+            String where= ContactsContract.RawContacts._ID + " = ? ";
+            String [] params= new String[] {targetString};
+
+            ContentResolver contentResolver=getContentResolver();
+            ContentValues contentValues=new ContentValues();
+            contentValues.put(ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY,newString);
+            contentResolver.update(ContactsContract.RawContacts.CONTENT_URI,contentValues, where,params);
+        }
+    }
+
+    private void removeContacts(){
+        String whereClause=ContactsContract.RawContacts.DISPLAY_NAME_PRIMARY+ " = '"+editTextContactName.getText().toString()+"'";
+        getContentResolver().delete(ContactsContract.RawContacts.CONTENT_URI,whereClause,null);
+    }
+
+    private void addContactsViaIntents(){
+        String tempContactText=editTextContactName.getText().toString();
+        if(tempContactText!=null && !tempContactText.equals("") && tempContactText.length()>0 ){
+            Intent intent=new Intent(ContactsContract.Intents.Insert.ACTION);
+            intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+            intent.putExtra(ContactsContract.Intents.Insert.NAME,tempContactText);
+            startActivity(intent);
+        }
+    }
+
+
+
+    /*private void loadContacts() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+            getLoaderManager().initLoader(1, null, this);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                Snackbar.make(findViewById(android.R.id.content),
+                        "Please Grant Permissions",
+                        Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS},MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+                            }
+                        }).show();
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
+    }*/
+
+
 
 }
